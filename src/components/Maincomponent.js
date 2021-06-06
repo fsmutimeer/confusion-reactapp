29a562e931aa3a20327d661831d3140373f78fa3
fsmutimeer <ishpata@hotmail.com>
2021-06-06 16:58:53 +0500

    Controlled forms

--- src/components/Maincomponent.js
+++ src/components/Maincomponent.js
@@ -4,6 +4,7 @@ import Header from './HeaderComponent';
 import Menu from '../components/Menu';
 import ContactComponent from './ContactComponent';
 
+
 import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
@@ -11,6 +12,7 @@ import { LEADERS } from '../shared/leaders';
 import { PROMOTIONS } from '../shared/promotions';
 import Footer from './FooterComponent';
 import {Switch, Route, Redirect} from 'react-router-dom';
+import About from './AboutComponent';
 
 
 class Main extends Component {
@@ -36,15 +38,26 @@ class Main extends Component {
             leader={this.state.leaders.filter((lead)=>lead.featured)[0]} />
         </div>
       )
-      
     }
+const DishWithId = ({match}) => {
+  return (
+    <DishDetail
+    dish={this.state.dishes.filter((dish)=>dish.id ===parseInt(match.params.dishId,10))[0]}
+    comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+    />
+  );
+
+}
+
   return (
     <div>
       <Header/>
       <Switch>
         <Route path='/home' component={HomePage}/>
         <Route exact path='/menu' component={()=> <Menu dishes={this.state.dishes}/>}/>
+        <Route path='/menu/:dishId' component={DishWithId} />
         <Route exact path='/contactus' component={ContactComponent}/>
+        <Route exact path='/aboutus' component={()=><About leaders={this.state.leaders}/>} />
        <Redirect to='/home'/>
       
       <DishDetail dish={this.state.dishes.filter((dish)=>dish.id === this.state.selectedDish)[0]}/>
